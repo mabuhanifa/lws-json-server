2bcf57977527a3be8fd4175cb169bcc92d45213d
shourovahmd <moh.abuhanifa@gmail.com>
2023-01-12 03:03:29 +0600

    first

---
+++ db.js
@@ -0,0 +1,13 @@
+const todos = require("./data/todos");
+const videos = require("./data/videos");
+const tags = require("./data/tags");
+const videos2 = require("./data/videos2");
+const transactions = require("./data/transactions");
+
+module.exports = {
+  todos,
+  videos,
+  videos2,
+  transactions,
+  tags,
+};
